b967f6f3b234ac0db4b3028c8230e06e7afab9bc
match <zsrscut@gmail.com>
2015-08-31 19:56:31 +0800

    Update db inspector.

--- inspect_db.sh
+++ inspect_db.sh
@@ -1,12 +1,11 @@
 #
-# pull-db
 # Inspect the database from your device
-# Cedric Beust
+# Match
 #
  
 PKG=me.zsr.feeder
 DB=feed_db
- 
+
 adb shell "run-as $PKG chmod 755 /data/data/$PKG/databases"
 adb shell "run-as $PKG chmod 666 /data/data/$PKG/databases/$DB"
 adb shell "rm /sdcard/$DB"
@@ -15,5 +14,11 @@ adb shell "cp /data/data/$PKG/databases/$DB /sdcard/$DB"
 rm -f ~/Temp/${DB}
 adb pull /sdcard/${DB} ~/Temp/${DB}
  
-# sqlitebrowser ~/Temp/${DB}
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	slitebrowser ~/Temp/${DB}
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	open -a sqlitebrowser ~/Temp/${DB}
+else
+	printf "Unknown os type : $OSTYPE"
+fi
 
