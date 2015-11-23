79161d61644e438172541a60ad0ac2fc7060d892
match <zsrscut@gmail.com>
2015-11-23 18:45:32 +0800

    Add share app to About.

--- app/src/main/java/me/zsr/feeder/other/AboutActivity.java
+++ app/src/main/java/me/zsr/feeder/other/AboutActivity.java
@@ -1,6 +1,7 @@
 package me.zsr.feeder.other;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
@@ -12,9 +13,19 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.simplelist.MaterialSimpleListAdapter;
+import com.afollestad.materialdialogs.simplelist.MaterialSimpleListItem;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.google.GooglePlus;
+import cn.sharesdk.sina.weibo.SinaWeibo;
+import cn.sharesdk.wechat.friends.Wechat;
+import cn.sharesdk.wechat.moments.WechatMoments;
 import me.zsr.feeder.BuildConfig;
 import me.zsr.feeder.R;
 import me.zsr.feeder.base.BaseActivity;
@@ -25,6 +36,7 @@ public class AboutActivity extends BaseActivity {
     private static final String URL_BUG = "https://github.com/zhangsr/Feeder/issues";
     private static final String URL_STORE = "http://www.wandoujia.com/apps/me.zsr.feeder";
     private static final String URL_AARON_SWARTZ = "https://en.wikipedia.org/wiki/Aaron_Swartz";
+    private static final String URL_SHARE = "http://fir.im/vdwa";
     @Bind(R.id.version_name_txt)
     TextView mVersionNameTextView;
     @Bind(R.id.info_img)
@@ -45,6 +57,8 @@ public class AboutActivity extends BaseActivity {
     ImageView mStoreImageView;
     @Bind(R.id.wechat_img)
     ImageView mWechatImageView;
+    @Bind(R.id.share_img)
+    ImageView mShareImageView;
     @Bind(R.id.toolbar)
     Toolbar mToolbar;
 
@@ -72,6 +86,7 @@ public class AboutActivity extends BaseActivity {
         mBugImageView.setColorFilter(getResources().getColor(R.color.main_grey_light));
         mStoreImageView.setColorFilter(getResources().getColor(R.color.main_grey_light));
         mWechatImageView.setColorFilter(getResources().getColor(R.color.main_grey_light));
+        mShareImageView.setColorFilter(getResources().getColor(R.color.main_grey_light));
     }
 
     @Override
@@ -101,6 +116,7 @@ public class AboutActivity extends BaseActivity {
             R.id.bug_layout,
             R.id.store_layout,
             R.id.aaron_swartz_layout,
+            R.id.share_layout,
     })
     public void layoutOnClick(View view) {
         switch (view.getId()) {
@@ -119,6 +135,79 @@ public class AboutActivity extends BaseActivity {
             case R.id.aaron_swartz_layout:
                 openUrl(URL_AARON_SWARTZ);
                 break;
+            case R.id.share_layout:
+                showShareMenu();
+                break;
         }
+    }
+
+    private void showShareMenu() {
+        final MaterialSimpleListAdapter adapter = new MaterialSimpleListAdapter(this);
+        adapter.add(new MaterialSimpleListItem.Builder(this)
+                .content(R.string.wechat)
+                .icon(R.drawable.ic_menu_wechat)
+                .build());
+        adapter.add(new MaterialSimpleListItem.Builder(this)
+                .content(R.string.moment)
+                .icon(R.drawable.ic_menu_moment)
+                .build());
+        adapter.add(new MaterialSimpleListItem.Builder(this)
+                .content(R.string.weibo)
+                .icon(R.drawable.ic_menu_weibo)
+                .build());
+        adapter.add(new MaterialSimpleListItem.Builder(this)
+                .content(R.string.google_plus)
+                .icon(R.drawable.ic_menu_google_plus)
+                .build());
+
+        new MaterialDialog.Builder(this)
+                .title(R.string.share_to)
+                .adapter(adapter, new MaterialDialog.ListCallback() {
+                    @Override
+                    public void onSelection(MaterialDialog dialog, View itemView, int which, CharSequence text) {
+                        switch (which) {
+                            case 0: {
+                                Wechat.ShareParams sp = new Wechat.ShareParams();
+                                sp.setShareType(Wechat.SHARE_WEBPAGE);
+                                sp.setTitle("Feeder");
+                                sp.setText(getString(R.string.app_share_desc));
+                                sp.setUrl(URL_SHARE);
+                                sp.setImageData(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher));
+                                Platform platform = ShareSDK.getPlatform(Wechat.NAME);
+                                platform.share(sp);
+                                break;
+                            }
+                            case 1: {
+                                WechatMoments.ShareParams sp = new WechatMoments.ShareParams();
+                                sp.setShareType(Wechat.SHARE_WEBPAGE);
+                                sp.setTitle("Feeder");
+                                sp.setText(getString(R.string.app_share_desc));
+                                sp.setUrl(URL_SHARE);
+                                sp.setImageData(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher));
+                                Platform platform = ShareSDK.getPlatform(WechatMoments.NAME);
+                                platform.share(sp);
+                                break;
+                            }
+                            case 2: {
+                                SinaWeibo.ShareParams sp = new SinaWeibo.ShareParams();
+                                sp.setText("Feeder, " + getString(R.string.app_share_desc));
+
+                                Platform platform = ShareSDK.getPlatform(SinaWeibo.NAME);
+                                platform.share(sp);
+                                break;
+                            }
+                            case 3: {
+                                GooglePlus.ShareParams sp = new GooglePlus.ShareParams();
+                                sp.setText("Feeder, " + getString(R.string.app_share_desc));
+
+                                Platform platform = ShareSDK.getPlatform(GooglePlus.NAME);
+                                platform.share(sp);
+                                break;
+                            }
+                        }
+                        dialog.dismiss();
+                    }
+                })
+                .show();
     }
 }
